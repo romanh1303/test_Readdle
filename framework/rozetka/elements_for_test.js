9772827f3aa9afd5671c69f20c048194dc75f89c
Andrey <kurkula.andrii@gmail.com>
2022-06-09 21:00:24 +0300

    test_example

---
+++ framework/rozetka/elements_for_test.js
@@ -0,0 +1,20 @@
+class Elements {
+  constructor() {
+    this.search_bar = '.search-form__input.ng-untouched.ng-pristine.ng-valid';
+    this.search_value = 'MD506Z/A';
+    this.product_page = '.suggest-goods';
+    this.add_value =
+      'button.buy-button.button.button--with-icon.button--green.button--medium.ng-star-inserted';
+    this.close_popup = '.modal__close';
+    this.logo_button = '.header__logo';
+    this.shoping_bag =
+      'button.header__button.ng-star-inserted.header__button--active';
+    this.expect_title =
+      'Apple MagSafe 2 85 Вт для MacBook Pro с 15" дисплеем Retina (MD506Z/A)   3 666 ₴ ';
+    this.expect_title1 =
+      'Apple MagSafe 2 85 Вт для MacBook Pro с 15" дисплеем Retina (MD506Z/A)';
+  }
+}
+module.exports = {
+  Elements,
+};
