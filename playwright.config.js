050bc67a0f7577d8f67480a5e60e1a420c173dc6
Andrey <kurkula.andrii@gmail.com>
2022-06-20 12:40:58 +0300

    chromium

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,17 @@
 
-
+const { devices } = require('@playwright/test');
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-  timeout: 20000,
+  timeout: 30000,
   use:{
     headless: false,
-  }
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ],
 };
 
 module.exports = config;
